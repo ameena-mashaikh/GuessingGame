11de0bdbf5106781b67a4f66660b4553f95b6f91
ameena-mashaikh <ameenamashaikh@gmail.com>
2023-05-22 11:16:24 -0400

    Phase 1 completed

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("Guess the Secret Number");
+string userAnswer = Console.ReadLine();
+Console.WriteLine("Your Guess: " + userAnswer);
