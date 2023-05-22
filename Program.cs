7f1d43cd3dc5ce823285614b51cf909e0b751317
ameena-mashaikh <ameenamashaikh@gmail.com>
2023-05-22 11:21:10 -0400

    phase 2 completed

--- Program.cs
+++ Program.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 
 Console.WriteLine("Guess the Secret Number");
-string userAnswer = Console.ReadLine();
-Console.WriteLine("Your Guess: " + userAnswer);
+string userInp = Console.ReadLine();
+int parsedInt = int.Parse(userInp);
+int secretNum = 42;
+if (parsedInt == secretNum)
+{
+    Console.WriteLine("Success! You guessed the Secret Number");
+}
+else
+{
+    Console.WriteLine("Sadly you guessed the wrong number");
+}
